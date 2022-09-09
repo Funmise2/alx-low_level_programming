fe7c9c7b23aecac35bf245c202c34d8045498059
Funmise2 <mcfalt243@gmail.com>
2022-09-08 20:08:17 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,7 @@
  *  * main - Print a text
  *   *
  *    * Return: Always 0 (Success)
- *     
- *           */
-
+ *     */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
