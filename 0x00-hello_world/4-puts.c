c5b8e8c2b815f80df9f3e68b6c764f6b0e7c9196
Funmise2 <mcfalt243@gmail.com>
2022-09-08 18:55:53 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /* This is a comment section */
-int main(){
+int main(void)
+{
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+			return (void);
 
 }
