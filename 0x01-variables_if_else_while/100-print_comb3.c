291fe2e879c8950bc7ca8a7c6a02c167f6671f45
Funmise2 <mcfalt243@gmail.com>
2022-09-09 20:16:36 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,27 +8,58 @@
  *
  */
 int main(void)
-{
-int d = 48, a = 48;
-	while (d < 58)
-		{
-		a = 48;
-			while (a < 58)
-				{
-				if (d != a && d < a)
-					{
-					putchar(d);
-					putchar(a);
 
-					if (!(d == 56 && a == 57))
-						{
-						putchar(' ');
-						}
-					}
-				a++;
-				}
-		d++;
-putchar('\n');
-	return (0);
+{
+
+		int d = 48, a = 48;
+
+
+
+			while (d < 58)
+
+					{
+
+								a = 48;
+
+										while (a < 58)
+
+													{
+
+																	if (d != a && d < a)
+
+																					{
+
+																										putchar(d);
+
+																														putchar(a);
+
+
+
+																																		if (!(d == 56 && a == 57))
+
+																																							{
+
+																																													putchar(',');
+
+																																																		putchar(' ');
+
+																																																						}
+
+																																					}
+
+																				a++;
+
+																						}
+
+												d++;
+
+													}
+
+				putchar('\n');
+
+
+
+					return (0);
+
 }
 
