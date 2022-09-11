35c102515b54d6c2c0eb76c8d4a17d3c709c2001
Funmise2 <mcfalt243@gmail.com>
2022-09-10 18:21:40 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@
  *
  *     The two numbers should be separated by a space.
  *
- *      All numbers should be printed with two digits. 1 should be printed as 01.
+ *  All numbers should be printed with two digits. 1 should be printed as 01.
  *
- *       Combination of numbers must be separated by a comma followed by a space.
+ *  Combination of numbers must be separated by a comma followed by a space.
  *
  *       Combinations of numbers should be printed in ascending order.
  *
