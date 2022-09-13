e36f01b7787b8ba58e1252cd32d30075f6315a9b
Funmise2 <mcfalt243@gmail.com>
2022-09-13 15:01:16 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,32 +1,22 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-void num(int r, int n);
-#endif
-
-#include <unistd.h>
+#include "_putchar.h"
 
 /**
- *  _putchar - writes the character c to stdout
- *  @c: The character to print
+ * main - Entry point
  *
- *  Return: On success 1.
- *  On error, -1 is returned, and errno is set appropriately.
+ *
+ *
+ * Return: Always 0 (Success)
+ *
  */
-
-int _putchar(char c)
+int main(void)
 {
-	  return (write(1, &c, 1));
+char C = "_putchar";
+while (C)
+{
+putchar(C);
+C++;
 }
+putchar('\n');
+return (0);
+}
+
