0ca85a43ba9179d4330fd7a433aa4c39710dbd9d
Funmise2 <mcfalt243@gmail.com>
2022-09-13 13:38:50 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,7 +7,7 @@
  * Return: Always 0 (Success)
  *
  */
-int main(void)
+int _putchar(char z)
 {
 char z = "_putchar";
 while (z)
