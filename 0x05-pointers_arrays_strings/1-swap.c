be6ca59eb8331c7553d3bbcef17ec160feed37fb
Funmise2 <mcfalt243@gmail.com>
2022-09-19 16:45:12 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,9 +6,8 @@
  *    @a: input parameter 1
  *    @b: input parameter 2
  *    Return: Nothing
- */                                            
-                                                            
-void swap_int(int *a, int *b)                                   
+ */
+void swap_int(int *a, int *b)
 {
 int temp;
 temp = *a;
