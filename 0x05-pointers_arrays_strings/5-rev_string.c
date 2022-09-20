58160ed093d05924793d8ec8820d0e19138ab047
Funmise2 <mcfalt243@gmail.com>
2022-09-20 16:03:51 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,17 @@
  * @s: string parameter input
  * Return: Nothing
  */
-
 void rev_string(char *s)
 {
-int l,i;
+int l, i;
 char ch;
 /*find string length without null char*/
-for (l = 0; s[l] != '\0'; ++l);
-/*swap the string by looping to half of the string*/
+for (l = 0; s[l] != '\0'; ++l);                                   
+/*swap the string by looping to half the string*/
 for (i = 0; i < l / 2; ++i)
 {
-ch = s[1];
-s[i] = s[l -1 - i]; /*Because the arrays starts from 0*/
+ch = s[i];
+s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
 s[l - 1 - i] = ch;
 }
 }
