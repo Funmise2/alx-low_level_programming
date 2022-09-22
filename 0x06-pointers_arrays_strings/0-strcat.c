608186783cd8139fc7c726c759de66c46cbf33ee
Funmise2 <mcfalt243@gmail.com>
2022-09-22 13:54:05 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,10 +12,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-int index = 0, dest_len = 0;
-while(dest[index++])
-dest_len++;
-for (index = 0;src[index]; index++)
-dest[dest_len++] = src[index];
+int b, d;
+b = d = 0;
+while (*(dest + b))
+b++;
+while ((*(dest + b) = *(src + d)))
+{
+b++;
+d++;
+}
 return (dest);
 }
