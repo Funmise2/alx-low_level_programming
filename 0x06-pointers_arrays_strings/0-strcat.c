8dc0d8160165bb7930af6ad0f089034ced9734d5
Funmise2 <mcfalt243@gmail.com>
2022-09-23 06:40:29 -0700

    fixed betty style

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,21 +2,26 @@
 #include <stdio.h>
 /**
  * _strcat - concatenates the string pointed to by @src to
- * the end of the string pointed to by@dest
+ * the end of the string pointed to by @dest
+ *
  * @dest: string that will be appended
- * @scr:strings to be concatenated upo
- * Return:returns pointer to @ dest
+ * @scr: strings to be concatenated upon
+ *
+ * Return: returns pointer to @ dest
  */
 char *_strcat(char *dest, char *src)
 {
-int b , d;
-b = d = 0;
-while (*(dest + b))
-b++;
-while ((*(dest + b) = *(src + d)))
-{
-b++;
-d++;
-}
-return (dest);
+	int b, d;
+
+	b = d = 0;
+
+	while (*(dest + b))
+		b++;
+
+		while ((*(dest + b) = *(src + d)))
+		{
+			b++;
+			d++;
+		}
+	return (dest);
 }
