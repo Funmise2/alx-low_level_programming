084b2f50b8b6c98152901eb1083c3308341acf73
Funmise2 <mcfalt243@gmail.com>
2022-09-23 07:06:24 -0700

    Fixed betty style errors

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,30 +1,26 @@
 #include "main.h"
-#include <stdio>
-
-/**
+ /**
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
+ *
  * Return: outputs the difference of s1 and s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
 int i = 0, r;
-/**
- *
- * 	 iterate through src and
- *
- * 	compare it with dest
- *
- */
+
+/* iterate through src and */
+
+/* compare it with dest */
+
 while (s1[i] != '\0' && s2[i] != '\0')
 {
-/**
- *
- * if they differ by a single character
- * don't iterate further
- */
+
+ /* if they differ by a single character */
+ /* don't iterate further */
+
 if (s1[i] != s2[i])
 {
 r = s1[i] - s2[i];
