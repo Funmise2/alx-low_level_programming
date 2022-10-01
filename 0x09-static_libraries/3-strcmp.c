86199231df1af6bbdbbba1c9435bc118068e62d9
Funmise2 <mcfalt243@gmail.com>
2022-09-30 22:11:50 -0700

    static lib

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+ /**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: outputs the difference of s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0, r;
+
+/* iterate through src and */
+
+/* compare it with dest */
+
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+
+ /* if they differ by a single character */
+ /* don't iterate further */
+
+if (s1[i] != s2[i])
+{
+r = s1[i] - s2[i];
+break;
+}
+else
+{
+r = s1[i] - s2[i];
+}
+i++;
+}
+return (r);
+}
